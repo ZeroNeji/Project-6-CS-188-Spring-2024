03742d5279c5b64f8a60cc789b92af94379d3732
ZeroNeji <144271727+ZeroNeji@users.noreply.github.com>
2025-05-11 00:05:46 -0500

    Add files

---
+++ model.py
@@ -0,0 +1,83 @@
+import nn
+
+class DeepQNetwork():
+    """
+    A model that uses a Deep Q-value Network (DQN) to approximate Q(s,a) as part
+    of reinforcement learning.
+    """
+    def __init__(self, state_dim, action_dim):
+        self.num_actions = action_dim
+        self.state_size = state_dim
+
+        # Remember to set self.learning_rate, self.numTrainingGames,
+        # self.parameters, and self.batch_size!
+        "*** YOUR CODE HERE ***"
+        self.learning_rate = 0.1
+        self.numTrainingGames = 12000
+        self.batch_size = 32
+
+        self.fc1_weights = nn.Parameter(state_dim, 128)
+        self.fc1_bias = nn.Parameter(1, 128)
+        self.fc2_weights = nn.Parameter(128, 64)
+        self.fc2_bias = nn.Parameter(1, 64)
+        self.fc3_weights = nn.Parameter(64, action_dim)
+        self.fc3_bias = nn.Parameter(1, action_dim)
+        self.parameters = [self.fc1_weights, self.fc1_bias, self.fc2_weights, self.fc2_bias, self.fc3_weights, self.fc3_bias]
+
+    def set_weights(self, layers):
+        self.parameters = []
+        for i in range(len(layers)):
+            self.parameters.append(layers[i])
+
+    def get_loss(self, states, Q_target):
+        """
+        Returns the Squared Loss between Q values currently predicted 
+        by the network, and Q_target.
+        Inputs:
+            states: a (batch_size x state_dim) numpy array
+            Q_target: a (batch_size x num_actions) numpy array, or None
+        Output:
+            loss node between Q predictions and Q_target
+        """
+        "*** YOUR CODE HERE ***"
+        Q_predictions = self.run(states)
+        return nn.SquareLoss(Q_predictions, Q_target)
+
+    def run(self, states):
+        """
+        Runs the DQN for a batch of states.
+        The DQN takes the state and returns the Q-values for all possible actions
+        that can be taken. That is, if there are two actions, the network takes
+        as input the state s and computes the vector [Q(s, a_1), Q(s, a_2)]
+        Inputs:
+            states: a (batch_size x state_dim) numpy array
+            Q_target: a (batch_size x num_actions) numpy array, or None
+        Output:
+            result: (batch_size x num_actions) numpy array of Q-value
+                scores, for each of the actions
+        """
+        "*** YOUR CODE HERE ***"
+        if not isinstance(states, nn.Constant):
+            states = nn.Constant(states)
+        
+        x = states
+        x = nn.ReLU(nn.AddBias(nn.Linear(x, self.fc1_weights), self.fc1_bias))
+        x = nn.ReLU(nn.AddBias(nn.Linear(x, self.fc2_weights), self.fc2_bias))
+        q_values = nn.AddBias(nn.Linear(x, self.fc3_weights), self.fc3_bias)
+        return q_values
+    
+    def gradient_update(self, states, Q_target):
+        """
+        Update your parameters by one gradient step with the .update(...) function.
+        Inputs:
+            states: a (batch_size x state_dim) numpy array
+            Q_target: a (batch_size x num_actions) numpy array, or None
+        Output:
+            None
+        """
+        "*** YOUR CODE HERE ***"
+        loss = self.get_loss(states, Q_target)
+        gradients = nn.gradients(loss, self.parameters)
+
+        for param, grad in zip(self.parameters, gradients):
+            param.update(grad, -self.learning_rate)
---
+++ valueIterationAgents.py
@@ -0,0 +1,151 @@
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+# valueIterationAgents.py
+# -----------------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+import mdp, util
+
+from learningAgents import ValueEstimationAgent
+import collections
+
+class ValueIterationAgent(ValueEstimationAgent):
+    """
+        * Please read learningAgents.py before reading this.*
+
+        A ValueIterationAgent takes a Markov decision process
+        (see mdp.py) on initialization and runs value iteration
+        for a given number of iterations using the supplied
+        discount factor.
+    """
+    def __init__(self, mdp: mdp.MarkovDecisionProcess, discount = 0.9, iterations = 100):
+        """
+          Your value iteration agent should take an mdp on
+          construction, run the indicated number of iterations
+          and then act according to the resulting policy.
+
+          Some useful mdp methods you will use:
+              mdp.getStates()
+              mdp.getPossibleActions(state)
+              mdp.getTransitionStatesAndProbs(state, action)
+              mdp.getReward(state, action, nextState)
+              mdp.isTerminal(state)
+        """
+        self.mdp = mdp
+        self.discount = discount
+        self.iterations = iterations
+        self.values = util.Counter() # A Counter is a dict with default 0
+        self.runValueIteration()
+
+    def runValueIteration(self):
+        """
+          Run the value iteration algorithm. Note that in standard
+          value iteration, V_k+1(...) depends on V_k(...)'s.
+        """
+        "*** YOUR CODE HERE ***"
+        states = self.mdp.getStates()
+        #compute Vk based on Vk-1
+        for i in range(self.iterations):
+            nextvalues = self.values.copy()
+            for state in states:
+                #if it is a terminal state, continue
+                if self.mdp.isTerminal(state):
+                    continue
+                actions = self.mdp.getPossibleActions(state)
+                #can't compute value if there are no actions
+                if not actions:
+                    continue
+                maxvalue = float('-inf')
+                for action in actions:
+                    qvalue = self.computeQValueFromValues(state, action)
+                    maxvalue = max(maxvalue, qvalue)
+                nextvalues[state] = maxvalue
+            self.values = nextvalues
+            
+
+
+
+    def getValue(self, state):
+        """
+          Return the value of the state (computed in __init__).
+        """
+        return self.values[state]
+
+    def computeQValueFromValues(self, state, action):
+        """
+          Compute the Q-value of action in state from the
+          value function stored in self.values.
+        """
+        "*** YOUR CODE HERE ***"
+        qvalue = 0
+        for nextState, probability in self.mdp.getTransitionStatesAndProbs(state, action):
+            reward = self.mdp.getReward(state, action, nextState)
+            qvalue += probability * (reward + self.discount*self.values[nextState])
+
+        return qvalue
+        
+
+    def computeActionFromValues(self, state):
+        """
+          The policy is the best action in the given state
+          according to the values currently stored in self.values.
+
+          You may break ties any way you see fit.  Note that if
+          there are no legal actions, which is the case at the
+          terminal state, you should return None.
+        """
+        "*** YOUR CODE HERE ***"
+        #terminal state
+        if self.mdp.isTerminal(state):
+            return None
+        actions = self.mdp.getPossibleActions(state)
+        #no moves
+        if not actions:
+            return None
+        #start max_value with a large negative number that is easily beat
+        #start best action with None because it doesn't know what the best move is yet
+        maxvalue = float('-inf')
+        bestaction = None
+        #if an action has a better value than maxvalue, set maxvalue to its value
+        #and bestaction to the action
+        for action in actions:
+            qvalue = self.computeQValueFromValues(state, action)
+            if qvalue > maxvalue:
+                maxvalue = qvalue
+                bestaction = action
+
+        return bestaction
+        
+        util.raiseNotDefined()
+
+    def getPolicy(self, state):
+        return self.computeActionFromValues(state)
+
+    def getAction(self, state):
+        "Returns the policy at the state (no exploration)."
+        return self.computeActionFromValues(state)
+
+    def getQValue(self, state, action):
+        return self.computeQValueFromValues(state, action)
